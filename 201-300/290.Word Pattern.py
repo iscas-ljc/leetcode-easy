49800d48f0d0568a8f129ff25beb7cf3af63a0d3
brodan <861218470@qq.com>
2017-09-21 13:39:01 +0800

    201-300

---
+++ 201-300/290.Word Pattern.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        s = pattern
+        t = str.split()
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t)) and len(s) == len(t)
+        
