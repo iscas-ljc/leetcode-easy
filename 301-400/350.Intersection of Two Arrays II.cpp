98ed81dac349d6ffef858e7ac94e07699a566d13
brodan <861218470@qq.com>
2017-09-25 16:35:48 +0800

    301-400

---
+++ 301-400/350.Intersection of Two Arrays II.cpp
@@ -0,0 +1,22 @@
+	class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+     	vector<int> result;
+     	sort(nums2.begin(),nums2.end());
+     	sort(nums1.begin(),nums1.end());
+     	for(int i=0,j=0;i<nums1.size()&&j<nums2.size()){
+     		if(nums1[i]==nums2[j]){
+     			result.push_back(nums1[i]);
+     			i++;
+     			j++;
+     		}
+     		else if(nums1[i]<nums2[j]){
+     			i++;
+     		}
+     		else if(nums1[i]>nums2[j]){
+     			j++;
+     		}
+     	}   
+     	return result;
+    }
+};
