1c96e5cc0f6cb79bf91a13c03d39a1b9a65be7c7
brodan <861218470@qq.com>
2017-10-13 14:38:47 +0800

    401-500

---
+++ 401-500/447.Number of Boomerangs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numberOfBoomerangs(self, points):
+        res = 0
+        for p in points:
+            cmap = {}
+            for q in points:
+                f = p[0]-q[0]
+                s = p[1]-q[1]
+                cmap[f*f + s*s] = 1 + cmap.get(f*f + s*s, 0)
+            for k in cmap:
+                res += cmap[k] * (cmap[k] -1)
+        return res
+#{}表示python中的dictionary
+#get表示查找key，','后为没找到的返回值
