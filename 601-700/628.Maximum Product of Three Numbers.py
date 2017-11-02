10d450bf6d4f094a6bed97fca32210d684b38efe
brodan <861218470@qq.com>
2017-11-02 15:32:15 +0800

    601-700

---
+++ 601-700/628.Maximum Product of Three Numbers.py
@@ -0,0 +1,4 @@
+class Solution(object):
+    def maximumProduct(self, nums):
+        nums.sort()
+        return max(nums[0]*nums[1]*nums[len(nums)-1],nums[len(nums)-1]*nums[len(nums)-2]*nums[len(nums)-3])
